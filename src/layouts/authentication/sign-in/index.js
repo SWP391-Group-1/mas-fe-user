715e6f8e99cf89b31ce2e11e08276de2f84fbce7
Ho Kim <nguyenhuuhuy203@gmail.com>
2022-06-09 13:01:48 +0700

    Add signin error message

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -1,122 +1,183 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 // react-router-dom components
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 // @mui material components
-import Switch from "@mui/material/Switch";
+import Switch from '@mui/material/Switch'
 
 // Soft UI Dashboard React components
-import SuiBox from "components/SuiBox";
-import SuiTypography from "components/SuiTypography";
-import SuiInput from "components/SuiInput";
-import SuiButton from "components/SuiButton";
+import SuiBox from 'components/SuiBox'
+import SuiTypography from 'components/SuiTypography'
+import SuiInput from 'components/SuiInput'
+import SuiButton from 'components/SuiButton'
 
 // Authentication layout components
-import CoverLayout from "layouts/authentication/components/CoverLayout";
+import CoverLayout from 'layouts/authentication/components/CoverLayout'
 
 // Images
-import curved9 from "assets/images/curved-images/curved-6.jpg";
-import Login from "examples/LoginGoogleButton";
+import curved9 from 'assets/images/curved-images/curved-6.jpg'
+import Login from 'examples/LoginGoogleButton'
 
 import { useNavigate } from 'react-router-dom'
 
 import { authApis } from '../../../apis/authApis'
 
 function SignIn() {
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [rememberMe, setRememberMe] = useState(true);
-  const navigate = useNavigate();
+    const [username, setUsername] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorPassword, setErrorPassword] = useState(null)
 
-  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+    const [password, setPassword] = useState(null)
+    const [rememberMe, setRememberMe] = useState(true)
+    const navigate = useNavigate()
 
-  const handleSignInButtonClick = () => {
-      authApis.loginAdmin(username, password)
-        .then(res => {
-            alert("Signed in successfully");
-            const token = res?.data?.message;
-            localStorage.setItem("access-token", token);
-            navigate("/dashboard");
-        })
-        .catch(err => {
-            console.error("Sign in failed.", err)
-        })
-    //   console.log("sign in meow", { username, password });
-  }
+    const handleSetRememberMe = () => setRememberMe(!rememberMe)
 
-  return (
-      <CoverLayout
-          title="FPT MAS"
-          description="Enter your email and password to sign in"
-          image={curved9}
-      >
-          <SuiBox component="form" role="form">
-              <SuiBox mb={2}>
-                  <SuiBox mb={1} ml={0.5}>
-                      <SuiTypography
-                          component="label"
-                          variant="caption"
-                          fontWeight="bold"
-                      >
-                          Email
-                      </SuiTypography>
-                  </SuiBox>
-                  <SuiInput type="email" placeholder="Email" onChange={e => setUsername(e?.target?.value)}/>
-              </SuiBox>
-              <SuiBox mb={2}>
-                  <SuiBox mb={1} ml={0.5}>
-                      <SuiTypography
-                          component="label"
-                          variant="caption"
-                          fontWeight="bold"
-                      >
-                          Password
-                      </SuiTypography>
-                  </SuiBox>
-                  <SuiInput type="password" placeholder="Password" onChange={e => setPassword(e?.target?.value)}/>
-              </SuiBox>
-              <SuiBox display="flex" alignItems="center">
-                  <Switch checked={rememberMe} onChange={handleSetRememberMe} />
-                  <SuiTypography
-                      variant="button"
-                      fontWeight="regular"
-                      onClick={handleSetRememberMe}
-                      sx={{ cursor: 'pointer', userSelect: 'none' }}
-                  >
-                      &nbsp;&nbsp;Remember me
-                  </SuiTypography>
-              </SuiBox>
-              <SuiBox mt={4} mb={1}>
-                  <SuiButton variant="gradient" color="info" fullWidth onClick={handleSignInButtonClick}>
-                      sign in
-                  </SuiButton>
-              </SuiBox>
-              <SuiBox mt={4} mb={1}>
-                  <Login/>
-              </SuiBox>
-              <SuiBox mt={3} textAlign="center">
-                  <SuiTypography
-                      variant="button"
-                      color="text"
-                      fontWeight="regular"
-                  >
-                      Don&apos;t have an account?{' '}
-                      <SuiTypography
-                          component={Link}
-                          to="/authentication/sign-up"
-                          variant="button"
-                          color="info"
-                          fontWeight="medium"
-                          textGradient
-                      >
-                          Sign up
-                      </SuiTypography>
-                  </SuiTypography>
-              </SuiBox>
-          </SuiBox>
-      </CoverLayout>
-  )
+    const handleSignInButtonClick = () => {
+        authApis
+            .loginAdmin(username, password)
+            .then((res) => {
+                const token = res?.data?.message
+                localStorage.setItem('access-token', token)
+                navigate('/dashboard')
+            })
+            .catch((err) => {
+                setErrorMessage(err.response.data.errors[0])
+                setErrorEmail(err.response.data.errors['Email'][0])
+                setErrorPassword(err.response.data.errors['Password'][0])
+                console.error('Sign in failed.', err.response.data.errors[0])
+            })
+        //   console.log("sign in meow", { username, password });
+    }
+
+    return (
+        <CoverLayout
+            title="FPT MAS"
+            description="Enter your email and password to sign in"
+            image={curved9}
+        >
+            <SuiBox component="form" role="form">
+                {errorMessage && (
+                    <SuiBox mb={1} ml={0.5}>
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                            color="error"
+                        >
+                            {errorMessage}
+                        </SuiTypography>
+                    </SuiBox>
+                )}
+                <SuiBox mb={2}>
+                    {errorEmail && (
+                        <SuiBox mb={1} ml={0.5}>
+                            <SuiTypography
+                                component="label"
+                                variant="caption"
+                                fontWeight="bold"
+                                color="error"
+                            >
+                                {errorEmail}
+                            </SuiTypography>
+                        </SuiBox>
+                    )}
+                    <SuiBox mb={1} ml={0.5}>
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                            isRequired
+                        >
+                            Email
+                        </SuiTypography>
+                    </SuiBox>
+                    <SuiInput
+                        type="email"
+                        placeholder="Email"
+                        onChange={(e) => setUsername(e?.target?.value)}
+                    />
+                </SuiBox>
+                <SuiBox mb={2}>
+                    {errorPassword && (
+                        <SuiBox mb={1} ml={0.5}>
+                            <SuiTypography
+                                component="label"
+                                variant="caption"
+                                fontWeight="bold"
+                                color="error"
+                            >
+                                {errorPassword}
+                            </SuiTypography>
+                        </SuiBox>
+                    )}
+                    <SuiBox mb={1} ml={0.5}>
+                        <SuiTypography
+                            component="label"
+                            variant="caption"
+                            fontWeight="bold"
+                            isRequired
+                        >
+                            Password
+                        </SuiTypography>
+                    </SuiBox>
+                    <SuiInput
+                        type="password"
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e?.target?.value)}
+                    />
+                </SuiBox>
+                <SuiBox display="flex" alignItems="center">
+                    <Switch
+                        checked={rememberMe}
+                        onChange={handleSetRememberMe}
+                    />
+                    <SuiTypography
+                        variant="button"
+                        fontWeight="regular"
+                        onClick={handleSetRememberMe}
+                        sx={{ cursor: 'pointer', userSelect: 'none' }}
+                    >
+                        &nbsp;&nbsp;Remember me
+                    </SuiTypography>
+                </SuiBox>
+                <SuiBox mt={4} mb={1}>
+                    <SuiButton
+                        variant="gradient"
+                        color="info"
+                        fullWidth
+                        onClick={handleSignInButtonClick}
+                    >
+                        sign in
+                    </SuiButton>
+                </SuiBox>
+                <SuiBox mt={4} mb={1}>
+                    <Login />
+                </SuiBox>
+                <SuiBox mt={3} textAlign="center">
+                    <SuiTypography
+                        variant="button"
+                        color="text"
+                        fontWeight="regular"
+                    >
+                        Don&apos;t have an account?{' '}
+                        <SuiTypography
+                            component={Link}
+                            to="/authentication/sign-up"
+                            variant="button"
+                            color="info"
+                            fontWeight="medium"
+                            textGradient
+                        >
+                            Sign up
+                        </SuiTypography>
+                    </SuiTypography>
+                </SuiBox>
+            </SuiBox>
+        </CoverLayout>
+    )
 }
 
-export default SignIn;
+export default SignIn
